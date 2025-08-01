d32b556f2169ba00a38932ab62935b82d397b463
yordanossole <yordanos.sole@gmail.com>
2025-08-01 23:38:08 +0300

    The attribute data is updated to handle str

--- src/dto/response.py
+++ src/dto/response.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Union
 
 class ApiResponse(BaseModel):
     message: Optional[str] = ""
-    data: Optional[dict] = None
+    data: Union[dict, str, None] = None
